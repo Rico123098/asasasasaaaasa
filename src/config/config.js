3a8bd911a97fc22f3af6c9de026e7298f5f51165
Rico123098 <67092552+Rico123098@users.noreply.github.com>
2021-02-22 13:30:28 +0100

    First

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "token": "ODEyMjU2ODkzOTQyMTA0MDk0.YC-HLg.FaFj_wfvlqsIuhYsnZMMDKO9t_I",
+    "token": "ODEyMjU2ODkzOTQyMTA0MDk0.YC-HLg.NuB5iNvpEQVvDW_K-Hh8jiMqET4",
     "prefix": "-",
     "corona_api": "https://corona.lmao.ninja/v3/covid-19/",
     "owner": "649324870655803403", 
